9ae6abaa85e22955837c91f86c3369f5ada3848f
Ali Sina <ali.sina1447@gmail.com>
2024-12-17 23:00:04 -0500

    initial

--- obj/Debug/net8.0/dotnet-practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/dotnet-practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63fc7574e4ab9f977fe4c99ebe72b0a743339d9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
