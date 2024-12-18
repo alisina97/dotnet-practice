63fc7574e4ab9f977fe4c99ebe72b0a743339d9a
Ali Sina <ali.sina1447@gmail.com>
2024-12-17 22:57:42 -0500

    initial

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.test
+// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. test
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
